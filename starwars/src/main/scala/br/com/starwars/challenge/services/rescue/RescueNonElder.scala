22cd355792bd88de8af547c3507387174757ecfe
MarceloIlegra <marcelo.serpa@ilegra.com>
2016-04-24 12:30:46 -0300

    replace if and else to pattern matching

--- starwars/src/main/scala/br/com/starwars/challenge/services/rescue/RescueNonElder.scala
+++ starwars/src/main/scala/br/com/starwars/challenge/services/rescue/RescueNonElder.scala
@@ -9,12 +9,14 @@ class RescueNonElder{
   def rescue(people: List[Person], vehicle: Vehicle) = {
     var nonEldersTravels = new ListBuffer[Travel]()
 
-    getNonElders(people).groupBy(person => person.urlSpecie).foreach{ case (specie, people) => people.foreach(p => addNonEldersTravels(p, specie)) }
+    getNonElders(people).groupBy(person => person.urlSpecie).foreach{ case (specie, people) => people.foreach({ person => addNonEldersTravels(person, specie)})}
 
     def addNonEldersTravels(person: Person, specie: String) = {
       val travelAvailable: ListBuffer[Travel] = nonEldersTravels.filter(t => t.specie == specie && t.itHasFreePlace(person))
-      if(travelAvailable.size > 0) travelAvailable(0).add(person)
-      else nonEldersTravels += new Travel(vehicle, ListBuffer[Person](person), "low", specie)
+      travelAvailable match {
+        case IsTravelAvailableListExtractor() => travelAvailable.head.add(person)
+        case _ => nonEldersTravels += new Travel(vehicle, ListBuffer[Person](person), "low", specie)
+      }
     }
     nonEldersTravels.toList
 
@@ -22,5 +24,11 @@ class RescueNonElder{
 
   def getNonElders(allPeople: List[Person]) =
     (allPeople.groupBy(p => p.urlSpecie) map { case (urlSpecie, people) => people.sortBy(p => p).tail} toList).flatten
+
+}
+
+object IsTravelAvailableListExtractor {
+
+  def unapply(travelAvailable: ListBuffer[Travel]): Boolean = travelAvailable.size > 0
 
 }
